87bfea159b4034ea3759a0f99b1eb70b9d24f0de
Nellefb <ellenfebar@gmail.com>
2026-09-08 18:39:34 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,10 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New Id = " + newSeller.getId());
 
+        System.out.println("\n==== TEST 5: Seller Update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update Completed");
     }
 }
